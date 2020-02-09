a34622727a74e5fe3918283a6d5b23e0bce259fe
Jlalla94 <46802387+Jlalla94@users.noreply.github.com>
2020-02-09 23:38:41 +0200

    todo
    todo commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import $ from "jquery";
 
+let activeItemsValue = 0;
+
 function ToDoItem({ text: text, id: id = " ", status: status = "" }) {
+  activeItemsValue = status !== "" ? activeItemsValue : activeItemsValue++;
   let viewItem = $(`
   <li class="${status}">
     <div class="view">
@@ -13,8 +16,15 @@ function ToDoItem({ text: text, id: id = " ", status: status = "" }) {
   viewItem.find(".toggle").on("change", function(event) {
     if ($(event.target).is(":checked")) {
       viewItem.addClass("completed");
+      $(".todo-count")
+        .find("strong")
+        .text(activeItemsValue--);
     } else {
       viewItem.removeClass("completed");
+      $("#toggle-all").prop("checked", false);
+      $(".todo-count")
+        .find("strong")
+        .text(activeItemsValue++);
     }
   });
   $('ul[class="todo-list"]').append(viewItem);
@@ -42,3 +52,29 @@ $(".new-todo")
       $(event.target).val("");
     }
   });
+
+$("#toggle-all").on("change", function(event) {
+  if ($(event.target).is(":checked")) {
+    $('ul[class="todo-list"]')
+      .children()
+      .each((index, item) => {
+        if (
+          !$(item)
+            .find(".toggle")
+            .is(":checked")
+        ) {
+          $(item)
+            .find(".toggle")
+            .click();
+        }
+      });
+  } else {
+    $('ul[class="todo-list"]')
+      .children()
+      .each((index, item) =>
+        $(item)
+          .find(".toggle")
+          .click()
+      );
+  }
+});
